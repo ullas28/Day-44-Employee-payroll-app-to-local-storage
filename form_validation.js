70e0aca165b7c4b3a19239fdf74392d258cbc22a
unknown <you@example.com>
2022-12-05 15:34:01 +0530

    Added : payroll object on save

--- form_validation.js
+++ form_validation.js
@@ -22,3 +22,54 @@ window.addEventListener('DOMContentLoaded', (event) => {
       output.textContent = salary.value;
     });
 });
+
+const save = () => {
+    try {
+      let employeePayrollData = createEmployeePayroll();
+      createAndUpdateStorage(employeePayrollData);
+    }
+    catch (e) {
+      return;
+    }
+  }
+  const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+      employeePayrollData.name = getInputValueById('#name');
+    }
+    catch (e) {
+      setTextValue('text-error', e);
+      throw e;
+    }
+  
+    employeePayrollData.profileImage = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById("#salary");
+    employeePayrollData.notes = getInputValueById("#notes");
+  
+    let date = getInputValueById("#year") + "-" + getInputValueById("#month") + "-" + getInputValueById("#day");
+    employeePayrollData.startDate = new Date(Date.parse(date));
+  
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+  }
+  
+  const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach((item) => {
+      if (item.checked) selItems.push(item.value);
+    });
+    return selItems;
+  };
+  
+  const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+  };
+  
+  const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+  };
